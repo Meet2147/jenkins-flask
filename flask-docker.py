4ae060320dda2ef445858803b5e7e68819723383
Meet Jethwa <meetjethwa3@gmail.com>
2019-06-12 15:48:02 +0530

    Update flask-docker.py

--- flask-docker.py
+++ flask-docker.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Changed it up a bit!the second time'
+    return 'Changed it up a bit!the third time'
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0:5000')
+    app.run(debug=True,host='0.0.0.0')
